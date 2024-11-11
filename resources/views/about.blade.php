ffbac8e7ef057e9f5b5f1629d536f5f7ea5edf1b
sena606 <gentasn@gmail.com>
2024-11-11 17:52:25 +0700

    03 Struktur Folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" href="css/style.css">
+    <title>Halaman about</title>
+</head>
+<body>
+    <a href="/">Home</a>
+   <a href="/about">About</a>
+<h1>Halaman About SN STORE</h1>
+<h3>Owner : {{ $nama }}</h3>
+<img src="img/logo.jpg" alt="logo_snstore" width="200">
+</body>
+</html>
